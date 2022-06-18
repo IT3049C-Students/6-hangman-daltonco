46ad082f8ddbaf4a7a3a3eab81a87b74c5cd16d8
daltonco <daltonco@mail.uc.edu>
2022-06-18 19:15:31 -0400

    Network Error is back

--- resources/js/Hangman.js
+++ resources/js/Hangman.js
@@ -20,7 +20,7 @@ class Hangman {
    * */
   getRandomWord(difficulty) {
     return fetch(
-      'https://hangman-micro-service.herokuapp.com/${difficulty}'
+      'https://hangman-micro-service.herokuapp.com/?${difficulty}'
     )
       .then((r) => r.json())
       .then((r) => r.word);
